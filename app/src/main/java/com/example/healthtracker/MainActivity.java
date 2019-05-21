c322678dfa5e4d4876a5f164b024604b50ebafc0
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-21 13:38:59 -0700

    day one feature 2

--- app/src/main/java/com/example/healthtracker/MainActivity.java
+++ app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.healthtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public int clickCounter = 0;
     public Button button;
     public TextView text_counter;
+
+    CarouselView carouselView;
+    public int[] carouselImageList = {R.drawable.begonia1, R.drawable.variegated2, R.drawable.magnolia3, R.drawable.moss4};
+//    CarouselView customCarouselView;
+//    int NUMBER_OF_PAGES = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 text_counter.setText(String.valueOf(clickCounter));
             }
         });
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(carouselImageList.length);
+
+        carouselView.setImageListener(imageListener);
+
     }
 
-//    public void onButtonClick(View view) {
-//        clickCounter = clickCounter+1;
-//        if(clickCounter == 1) {
-//        TextView text = findViewById(R.id.text_counter);
-//        text.setText("Click Count:" + clickCounter);
-//
-//        }
-//    }
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(carouselImageList[position]);
+        }
+    };
+
+
+
 }
