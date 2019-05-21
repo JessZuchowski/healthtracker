9fb57722b59527d5151ef8319f78916fcbdcfc52
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-21 10:47:59 -0700

    day 1 feature 1 complete

---
+++ app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.healthtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    public int clickCounter = 0;
+    public Button button;
+    public TextView text_counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text_counter = findViewById(R.id.text_counter);
+        button = findViewById(R.id.button);
+
+        text_counter.setText("0");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickCounter = clickCounter + 1;
+                text_counter.setText(String.valueOf(clickCounter));
+            }
+        });
+    }
+
+//    public void onButtonClick(View view) {
+//        clickCounter = clickCounter+1;
+//        if(clickCounter == 1) {
+//        TextView text = findViewById(R.id.text_counter);
+//        text.setText("Click Count:" + clickCounter);
+//
+//        }
+//    }
+}
